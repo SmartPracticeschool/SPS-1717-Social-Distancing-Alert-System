5e69068aa107309fbc750b8079958fafefbd3ee3
Altafsarfraz36 <68593938+Altafsarfraz36@users.noreply.github.com>
2020-08-21 17:04:06 +0530

    Add files via upload

---
+++ social_distancing_config.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 18 20:23:57 2020
+
+@author: HP
+"""
+
+MODEL_PATH = "yolo-coco"
+
+MIN_CONF = 0.3
+NMS_THRESH = 0.3
+
+USE_GPU = False
+
+MIN_DISTANCE = 50
+
